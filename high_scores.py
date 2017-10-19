cbbeebcb3f4de451456281d63597f6a935aeea9f
Sebastian <sebastian.halinski@gmail.com>
2017-10-19 16:41:10 +0200

    hall of fame table

--- high_scores.py
+++ high_scores.py
@@ -1,31 +1,76 @@
-def high_scores(name, points, text_file):  # jak obliczamy punkty???
-    with open(text_file, "r") as file:
-        lines = []
-        for line in file:
-            line = line.strip()
-            line = line.split(",")
-            lines.append(line)
-    new_score = [name, str(points)]
+def player_score(score, all_shots, level, name):
+    high_score_line = []
+    high_score_line.append(score)
+    high_score_line.append(all_shots)
+    high_score_line.append(level)
+    high_score_line.append(name)
+    return high_score_line
 
-    for i in range(len(lines)):
-        if points > int(lines[i][1]):
-            lines.insert(i, new_score)
-            lines.pop()
-            print(name, ", your score is:", points)
-            print("You will be added to the hall of fame!")
+
+def export_hall_of_fame(text_file, high_scores_data):
+    import csv
+
+    with open(text_file, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        for row in high_scores_data:
+            writer.writerow(row)
+
+
+def import_hall_of_fame(text_file, player_score):
+    import csv
+    high_scores_data = []
+    with open(text_file, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            row[0] = int(row[0])
+            high_scores_data.append(row)
+    high_scores_data = sorted(high_scores_data, key = lambda score: score[0], reverse = True)
+
+    for row in high_scores_data:
+        if int(row[0]) <= player_score[0]:
+            del high_scores_data[-1]
+
+            high_scores_data.append(player_score)
             break
-    else:
-        print(name, ", your score is", points)
-        print("You will not be added to hall of fame...")
-    input("Press enter to continue: ")
+    high_scores_data = sorted(high_scores_data, key = lambda score: score[0], reverse = True)
+
+    return high_scores_data
 
 
-    new_scores = ""
-    for line in lines:
-        new_scores += ",".join(line)+'\n'
-    with open(text_file, "w") as new_file:
-        new_file.write(new_scores)
+def get_highscore_table():
+    name_data = []
+    level_data = []
+    all_shots_data = []
+    score_data = []
+    index_data = []
+    index = 1
+
+    high_scores_data = import_hall_of_fame("high_scores.csv", player_score)
+
+    for i in high_scores_data:
+        index_data.append(str(index))
+        name_data.append(i[3])
+        level_data.append(i[1])
+        all_shots_data.append(str(i[2]))
+        score_data.append(str(i[0]))
+        index += 1
+
+    import texttable as tt
+    tab = tt.Texttable()
+    headings = ['Idx', 'Score', 'all shots', 'level', 'Name']
+    tab.header(headings)
+
+    for row in zip(index_data, score_data, level_data, all_shots_data, name_data):
+        tab.add_row(row)
+
+    table = tab.draw()
+
+    return str(table)
 
 
-high_scores("BattleSheep2", 1200, "high_scores.txt")
-
+# high_score_line = player_score(2379, 42, 'medium', 'plajer')
+# player_score = player_score(8999, 12, 'hard', 'plejer')
+# print(import_hall_of_fame('high_scores.csv', player_score))
+# high_scores_data = import_hall_of_fame('high_scores.csv', player_score)
+# print(get_highscore_table())
+# export_hall_of_fame('high_scores.csv', high_scores_data)
